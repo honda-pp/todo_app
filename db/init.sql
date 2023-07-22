13ff21cbf336dea982e08edded4384e9fea14f75
honda-pp <s.honda@keio.jp>
2023-07-22 19:36:13 +0900

    [change] status -> completed

--- db/init.sql
+++ db/init.sql
@@ -3,16 +3,16 @@ CREATE TABLE tasks (
     title VARCHAR(255) NOT NULL,
     description TEXT,
     due_date TIMESTAMP,
-    status VARCHAR(20) NOT NULL DEFAULT '未完了',
+    completed boolean NOT NULL DEFAULT false,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP
 );
 
-INSERT INTO tasks (title, description, due_date, status, created_at, updated_at)
-VALUES ('Finish report', 'Write a report for the project', '2023-07-25 15:00:00', 'In progress', NOW(), NOW());
+INSERT INTO tasks (title, description, due_date, completed, created_at, updated_at)
+VALUES ('Finish report', 'Write a report for the project', '2023-07-25 15:00:00', false, NOW(), NOW());
 
-INSERT INTO tasks (title, description, status, created_at, updated_at)
-VALUES ('Buy groceries', 'Milk, eggs, bread, and fruits', 'Not started', NOW(), NOW());
+INSERT INTO tasks (title, description, completed, created_at, updated_at)
+VALUES ('Buy groceries', 'Milk, eggs, bread, and fruits', false, NOW(), NOW());
 
-INSERT INTO tasks (title, description, due_date, status, created_at, updated_at)
-VALUES ('Prepare presentation', 'Create slides for the meeting', '2023-07-20 10:30:00', 'Completed', NOW(), NOW());
+INSERT INTO tasks (title, description, due_date, completed, created_at, updated_at)
+VALUES ('Prepare presentation', 'Create slides for the meeting', '2023-07-20 10:30:00', true, NOW(), NOW());
