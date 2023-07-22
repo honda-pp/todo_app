5fec6f0c442ad6988f0de125d70cb75bd66d5e22
honda-pp <s.honda@keio.jp>
2023-07-22 19:50:22 +0900

    [change] table name

--- backend/app/models/todo.go
+++ backend/app/models/todo.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type Todo struct {
-	ID          int       `json:"id"`
+	TaskID      int       `json:"task_id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
+	DueDate     time.Time `json:"due_date"`
 	Completed   bool      `json:"completed"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tasks (
+CREATE TABLE todo (
     task_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
@@ -8,11 +8,11 @@ CREATE TABLE tasks (
     updated_at TIMESTAMP
 );
 
-INSERT INTO tasks (title, description, due_date, completed, created_at, updated_at)
+INSERT INTO todo (title, description, due_date, completed, created_at, updated_at)
 VALUES ('Finish report', 'Write a report for the project', '2023-07-25 15:00:00', false, NOW(), NOW());
 
-INSERT INTO tasks (title, description, completed, created_at, updated_at)
+INSERT INTO todo (title, description, completed, created_at, updated_at)
 VALUES ('Buy groceries', 'Milk, eggs, bread, and fruits', false, NOW(), NOW());
 
-INSERT INTO tasks (title, description, due_date, completed, created_at, updated_at)
+INSERT INTO todo (title, description, due_date, completed, created_at, updated_at)
 VALUES ('Prepare presentation', 'Create slides for the meeting', '2023-07-20 10:30:00', true, NOW(), NOW());
