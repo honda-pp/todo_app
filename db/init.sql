44cd72aed1df004da60866d54ff8dc0208a37046
honda-pp <s.honda@keio.jp>
2023-07-22 19:11:10 +0900

    [add] sample data

--- db/init.sql
+++ db/init.sql
@@ -7,3 +7,12 @@ CREATE TABLE tasks (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP
 );
+
+INSERT INTO tasks (title, description, due_date, status, created_at, updated_at)
+VALUES ('Finish report', 'Write a report for the project', '2023-07-25 15:00:00', 'In progress', NOW(), NOW());
+
+INSERT INTO tasks (title, description, status, created_at, updated_at)
+VALUES ('Buy groceries', 'Milk, eggs, bread, and fruits', 'Not started', NOW(), NOW());
+
+INSERT INTO tasks (title, description, due_date, status, created_at, updated_at)
+VALUES ('Prepare presentation', 'Create slides for the meeting', '2023-07-20 10:30:00', 'Completed', NOW(), NOW());
