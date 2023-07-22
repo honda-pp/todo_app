40661017530a69eee7a628d19528d3b4bc20b1b2
honda-pp <s.honda@keio.jp>
2023-07-22 19:59:57 +0900

    [add] getTodoList

--- backend/app/models/todo.go
+++ backend/app/models/todo.go
@@ -1,13 +1,16 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Todo struct {
-	TaskID      int       `json:"task_id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	DueDate     time.Time `json:"due_date"`
-	Completed   bool      `json:"completed"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	TaskID      int            `json:"task_id"`
+	Title       string         `json:"title"`
+	Description sql.NullString `json:"description"`
+	DueDate     sql.NullTime   `json:"due_date"`
+	Completed   bool           `json:"completed"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   sql.NullTime   `json:"updated_at"`
 }
--- backend/app/repositories/todo_repository.go
+++ backend/app/repositories/todo_repository.go
@@ -1,6 +1,11 @@
 package repositories
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/honda-pp/todo_app/backend/app/models"
+	"github.com/honda-pp/todo_app/backend/infrastructure/logger"
+)
 
 type TodoRepository struct {
 	db *sql.DB
@@ -10,4 +15,41 @@ func NewTodoRepository(db *sql.DB) *TodoRepository {
 	return &TodoRepository{
 		db: db,
 	}
+}
+
+func (r *TodoRepository) GetTodoList() ([]*models.Todo, error) {
+
+	query := "SELECT task_id, title, description, due_date, completed, created_at, updated_at FROM todo"
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	todoList := make([]*models.Todo, 0)
+
+	for rows.Next() {
+		todo := &models.Todo{}
+		err := rows.Scan(
+			&todo.TaskID,
+			&todo.Title,
+			&todo.Description,
+			&todo.DueDate,
+			&todo.Completed,
+			&todo.CreatedAt,
+			&todo.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		logger.LogInfo("ddd")
+		todoList = append(todoList, todo)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return todoList, nil
 }
